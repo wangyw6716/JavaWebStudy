15c6de31b592a462dabc47d7149bdff45fbc0e0a
wangyw6716 <671682551@qq.com>
2021-07-09 14:44:40 +0800

    JavaWeb学习第二天下午

--- response/src/main/java/FileServlet.java
+++ response/src/main/java/FileServlet.java
@@ -12,11 +12,11 @@ public class FileServlet extends HttpServlet {
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 //        1. 要获取下载文件的路径
         String realPath = "D:\\Java\\workspace\\javaweb\\response\\src\\main\\resources\\daima.txt";
-        System.out.println("下载文件路径:"+realPath);
+        System.out.println("下载文件路径:" + realPath);
 //        2. 下载的文件名是啥?
-        String fileName = realPath.substring(realPath.lastIndexOf("\\")+1);
+        String fileName = realPath.substring(realPath.lastIndexOf("\\") + 1);
 //        3. 设置想办法让浏览器能够支持(Content-Disposition)下载我们需要的东西
-        resp.setHeader("Content-Disposition","attachment;filename"+ URLEncoder.encode(fileName,"UTF-8"));
+        resp.setHeader("Content-Disposition", "attachment;filename" + URLEncoder.encode(fileName, "UTF-8"));
 //        4. 获取下载文件的输入流
         FileInputStream in = new FileInputStream(realPath);
 //        5. 创建缓冲区
@@ -25,8 +25,8 @@ public class FileServlet extends HttpServlet {
 //        6. 获取OutputStream对象
         ServletOutputStream out = resp.getOutputStream();
 //        7. 讲FileOutputStream流写入到buffer缓冲区,使用OutputStream讲缓冲区中的数据输出到客户端
-        while ((len=in.read(buffer))>0){
-            out.write(buffer,0,len);
+        while ((len = in.read(buffer)) > 0) {
+            out.write(buffer, 0, len);
         }
         in.close();
         out.close();
